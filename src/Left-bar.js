0bad87fff4009506d785bb6d71a98c5f21c6f26c
admin <matthewosu@gmail.com>
2021-05-17 21:35:09 -0700

    first commit

---
+++ src/Left-bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LeftBar() {
+    return (
+        <div className="container-left-bar">
+          <div className="left-bar"><p>hi</p></div>
+        </div>
+    )
+
+}
+
+
+// export default LeftBar
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Title() {
+    return (
+        <div className="">
+          <div className="title"><p>Creative Crossroads</p></div>
+        </div>
+    )
+
+}
+
