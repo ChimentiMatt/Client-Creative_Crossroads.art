e39ee9ff31c42d323abc5b263dcef9e7c9f09c6a
admin <matthewosu@gmail.com>
2021-05-24 00:00:33 -0700

    flexed.intro.page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Centerpiece from './home.js';
 import FamilyPortraitPage from './Familyportrait.js';
 import PianoCamera from "./img-phot/camera-square.jpeg";
 import Logo from "./img-phot/Watermark.png";
+import LogoHeader from "./img-phot/Watermark-for-header.png";
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,7 +22,7 @@ import {
 } from "react-router-dom";
 
 
-    
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -34,93 +35,100 @@ class App extends React.Component {
   }
 
   makeActive(params) {
-      this.setState({
-        activeLink: params
-      })
+    this.setState({
+      activeLink: params
+    })
   }
 
   // function mActive(params) {
   //   const {  }
   // }
 
-  render() { 
+  render() {
 
     return (
 
       // Uess a Ternary webpage to landing page on first render 
-      this.state.intro ?  
+      this.state.intro ?
         <div className="intro-page">
-          <div className="intro-page-img" >
+          <div className="container-intro-texts">
 
+            <h1 className="intro-h1">CREATIVE CROSSROADS</h1>
+            <img className="watermark" src={LogoHeader} ></img>
+            <div className="container-rows">
+              <div className="row1-buttons">
+                <button className="intro-page-btn1" onClick={() => this.setState({ intro: false })}>Photography</button>
+                <button className="intro-page-btn2" onClick={() => this.setState({ intro: false })}>Weddings </button>
+                <button className="intro-page-btn3" onClick={() => this.setState({ intro: false })}>Headshots</button>
+              </div>
+              <div className="row2-buttons">
+                <button className="intro-page-btn4" onClick={() => this.setState({ intro: false })}>Family Portraits</button>
+                <button className="intro-page-btn5" onClick={() => this.setState({ intro: false })}>Musical Performances</button>
+              </div>
+            </div>
+            {/* Hidden button  */}
           </div>
-          <button className="intro-page-btn1" onClick={ ()=> this.setState({intro: false})}>Photography</button>
-          <button className="intro-page-btn2" onClick={ ()=> this.setState({intro: false})}>Weddings</button>
-          <button className="intro-page-btn3" onClick={ ()=> this.setState({intro: false})}>Headshots</button>
-          <button className="intro-page-btn4" onClick={ ()=> this.setState({intro: false})}>Family Portraits</button>
-          <button className="intro-page-btn5" onClick={ ()=> this.setState({intro: false})}>Musical Performances</button>
-
-          {/* Hidden button  */}
-          <button className="intro-page-btn-catch-all" onClick={ ()=> this.setState({intro: false})}>.</button>
-          <h1 className="intro-h1">CREATIVE CROSSROADS</h1>
-          <img className="watermark" src={Logo} ></img>
+            <button className="intro-page-btn-catch-all" onClick={() => this.setState({ intro: false })}>.</button>
+          <div className="intro-page-img"></div>
         </div>
 
-      : <div className="container-app">
-          
-  
+        : <div className="container-app">
 
-        <Router>
+
+
+          <Router>
             <div className="container-heading">
-                <Link className="logo" to="/home">CREATIVE CROSSROADS</Link>
+              <Link className="logo" to="/home">CREATIVE CROSSROADS</Link>
+              <div className="container-header-watermark">
+                <img className="header-watermark" src={LogoHeader} ></img>
+              </div>
+              <div className='container-a-tag'>
+                <Link className="links" to="/photography">PHOTOGRAPHY</Link>
 
+                <Link className="links" to="/wedding">WEDDING PHOTOGRAPHY</Link>
 
-                    <div className='container-a-tag'>
-                        <Link className="links" to="/photography">PHOTOGRAPHY</Link>
-        
-                        <Link className="links" to="/wedding">WEDDING PHOTOGRAPHY</Link>
+                <Link className="links" to="/headshots">HEADSHOTS</Link>
 
-                        <Link className="links" to="/headshots">HEADSHOTS</Link>
+                <Link id={this.state.active} className="links" to="/familyportrait">FAMILY PORTRAITS</Link>
 
-                        <Link id={this.state.active} className="links" to="/familyportrait">FAMILY PORTRAITS</Link>
-
-                        <Link className="links" to="/music">MUSICAL PERFORMANCES</Link>
-                    </div>
+                <Link className="links" to="/music">MUSICAL PERFORMANCES</Link>
+              </div>
 
             </div>
             <div className="content-body">
-                <Switch>
-                    <Route exact path="/home">
-                        <Centerpiece />
-                    </Route>
-                    <Route exact path="/photography">
-                        {/* <PhotogPage /> */}
+              <Switch>
+                <Route exact path="/home">
+                  <Centerpiece />
+                </Route>
+                <Route exact path="/photography">
+                  {/* <PhotogPage /> */}
 
+                  <div className="container-photos"> 
+                    <BasicRows />
+                  </div>
 
-                          <BasicRows /> 
+                </Route>
+                <Route exact path="/wedding">
+                  <WeddingPage />
+                </Route>
+                <Route exact path="/headshots">
 
+                </Route>
+                <Route exact path="/:familyportrait" >
+                  <FamilyPortraitPage makeActive={this.makeActive.bind(this)} />
 
-                    </Route>
-                    <Route exact path="/wedding">
-                        <WeddingPage />
-                    </Route>
-                    <Route exact path="/headshots">
+                </Route>
+                <Route exact path="/music">
 
-                    </Route>
-                    <Route exact path="/:familyportrait" >
-                        <FamilyPortraitPage makeActive={this.makeActive.bind(this)}/>
-                        
-                    </Route>
-                    <Route exact path="/music">
-
-                    </Route>
-                    <Route exact path="">
-                        <p>testing</p>
-                    </Route>
-                </Switch>
+                </Route>
+                <Route exact path="">
+                  <p>testing</p>
+                </Route>
+              </Switch>
             </div>
 
-        </Router>
-      </div>
+          </Router>
+        </div>
     );
   }
 }
