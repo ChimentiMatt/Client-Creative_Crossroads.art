9d382b1ed1840c93d30c34b85998b27ad26a2821
admin <matthewosu@gmail.com>
2021-07-20 16:49:49 -0700

    trying to figure out light box for images, added headshots

--- src/components/PhotographyPage.js
+++ src/components/PhotographyPage.js
@@ -29,24 +29,28 @@ import img23 from '../img-phot/exodus.jpeg';
 const photos = [
     {
         class: 'photo',
+        id: 0,
         src: img2,
         width: 4617,
         height: 3096
     },
     {
         class: 'photo',
+        id: 1,
         src: img9,
         width: 4400,
         height: 2094
     },
     {
         class: 'photo',
+        id: 2,
         src: img10,
         width: 3609,
         height: 1567
     },
     {
         class: 'photo',
+        id: 3,
         src: img11,
         width: 1600,
         height: 1068
@@ -54,18 +58,21 @@ const photos = [
     },
     {
         class: 'photo',
+        id: 4,
         src: img5,
         width: 5008,
         height: 3336
     },
     {
         class: 'photo',
+        id: 5,
         src: img7,
         width: 768,
         height: 960
     },
     {
         class: 'photo',
+        id: 6,
         src: img8,
         width: 5008,
         height: 3336
@@ -73,78 +80,91 @@ const photos = [
 
     {
         class: 'photo',
+        id: 7,
         src: img3,
         width: 5470,
         height: 3791
     },
     {
         class: 'photo',
+        id: 8,
         src: img12,
         width: 6000,
         height: 4000
     },
     {
         class: 'photo',
+        id: 9,
         src: img13,
         width: 960,
         height: 640
     },
     {
         class: 'photo',
+        id: 10,
         src: img14,
         width: 960,
         height: 646
     },
     {
         class: 'photo',
+        id: 11,
         src: img15,
         width: 3799,
         height: 2533
     },
     {
         class: 'photo',
+        id: 12,
         src: img1,
         width: 2048,
         height: 1365
     },
     {
         class: 'photo',
+        id: 13,
         src: img16,
         width: 3936,
         height: 2624
     },
     {
         class: 'photo',
+        id: 14,
         src: img17,
         width: 3936,
         height: 2624
     },
     {
         class: 'photo',
+        id: 15,
         src: img18,
         width: 2048,
         height: 1365
     },
     {
         class: 'photo',
+        id: 16,
         src: img19,
         width: 1772,
         height: 1340
     },
     {
         class: 'photo',
+        id: 17,
         src: img20,
         width: 1600,
         height: 1066
     },
     {
         class: 'photo',
+        id: 18,
         src: img21,
         width: 4030,
         height: 2881
     },
     {
         class: 'photo',
+        id: 19,
         src: img22,
         width: 2465,
         height: 1451
@@ -154,13 +174,14 @@ const photos = [
 // temporary function that shows sucessful selection of photo
 const logFun = event => {
     console.log('cats')
+    document.getElementsByClassName('photo')[0].style.height = '20px'
 }
 
 const BasicRows = () => 
 <Gallery 
-id='1' 
-photos={photos} 
-onClick={logFun}
+    id='1' 
+    photos={photos} 
+    onClick={logFun}
 />
 
 export default BasicRows
