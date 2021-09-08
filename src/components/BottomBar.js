a8932f2c4f4d138bf79412a2da34148e41b8d180
matthew chimenti <matthewosu@gmail.com>
2021-09-07 17:12:36 -0700

    removed sencond bootomBar.js as it was not used and removed isntagram for now

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -6,22 +6,25 @@ export default function BottomBar() {
     return (
         <div className="container-home">
             <div id="app"></div>
-  
-            <div className="footer">  
-                <div className="footer-text">   
+
+            <div className="footer">
+                <div className="footer-text">
                     <div className="footer-left-container">
-                        <p>CREATIVE CROSSROADS,</p> 
+                        <p>CREATIVE CROSSROADS,</p>
                         <p>SUZANNE CHIMENTI PHOTOGRAPHY</p>
                     </div>
-                    <p id="ContainerInstagramIcon" onClick={()=> window.open("https://www.instagram.com/suzannechim/", "_blank")}>
-                    <IconContext.Provider value={{ size: "2em", hover: "red"}}>
-                        <div id="instagramIcon">
-                            <FaInstagram/>
-                        </div>
-                    </IconContext.Provider>
-                </p>
-                    <p>ODYSSEY WEB DESIGN</p>
-                </div>  
+
+                    <div>
+                        <p>ODYSSEY WEB DESIGN</p>
+                        {/* <p id="ContainerInstagramIcon" onClick={() => window.open("https://www.instagram.com/suzannechim/", "_blank")}>
+                            <IconContext.Provider value={{ size: "2em", hover: "red" }}>
+                                <div id="instagramIcon">
+                                    <FaInstagram />
+                                </div>
+                            </IconContext.Provider>
+                        </p> */}
+                    </div>
+                </div>
             </div>
         </div>
     )
